8da9ada2014a32efe984f2a00134fb5123135050
Amr Elstohy <amramr2901@gmail.com>
2025-12-06 22:20:12 +0200

    Enhance auth middleware with allowed URL checks and consistent error messages
    - Added a list of allowed URLs to the auth middleware to bypass phone verification for specific routes.
    - Updated error messages to use single quotes for consistency throughout the middleware.
    - Improved the logic to check for phone verification only for non-allowed URLs, enhancing user access control while maintaining security.

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,13 +1,21 @@
-const { UnauthorizedError } = require("../utils/errors");
-const jwt = require("jsonwebtoken");
-const User = require("../features/users/user.model");
-require("dotenv").config();
+const { UnauthorizedError } = require('../utils/errors');
+const jwt = require('jsonwebtoken');
+const User = require('../features/users/user.model');
+require('dotenv').config();
+const allowedUrls = [
+  '/api/v1/auth/send-phone-otp',
+  '/api/v1/auth/verify-phone-otp',
+  '/api/v1/auth/send-reset-password-otp',
+  '/api/v1/auth/verify-reset-password-otp',
+  '/api/v1/auth/reset-password',
+  '/api/v1/auth/logout',
+];
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-    throw new UnauthorizedError("Missing token");
+    throw new UnauthorizedError('Missing token');
   }
 
   let userId;
@@ -16,26 +24,28 @@ const authMiddleware = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     userId = decoded.userId;
   } catch (error) {
-    throw new UnauthorizedError("Invalid token");
+    throw new UnauthorizedError('Invalid token');
   }
 
   const user = await User.findById(userId);
 
   if (!user) {
-    throw new UnauthorizedError("User not found");
+    throw new UnauthorizedError('User not found');
   }
 
   if (!user.refreshToken) {
-    throw new UnauthorizedError("This user is not logged in");
+    throw new UnauthorizedError('This user is not logged in');
   }
 
-  if (user.type !== req.appType && user.type !== "admin") {
-    throw new UnauthorizedError("This user is not allowed to access this app");
+  if (user.type !== req.appType && user.type !== 'admin') {
+    throw new UnauthorizedError('This user is not allowed to access this app');
   }
 
-  // if ( user.isPhoneVerified === false) {
-  //   throw new UnauthorizedError("Phone number is not verified");
-  // }
+  if (!allowedUrls.includes(req.originalUrl)) {
+    if (user.isPhoneVerified === false) {
+      throw new UnauthorizedError('Phone number is not verified');
+    }
+  }
 
   req.user = user;
 
